a8954712c136679cbfd90b002d02e6cee5f3ec32
MarkRefTeam <szllsy.mark@gmail.com>
2025-02-20 11:38:54 +0100

    Fixed input message and added return explanation

--- Practice/src/FirstLoop.java
+++ Practice/src/FirstLoop.java
@@ -6,22 +6,23 @@ public class FirstLoop {
 
         System.out.println("It will be my first loop try");
 
-        String text = scanner.nextLine();
         System.out.println("Enter your name");
+        String text = scanner.nextLine();
 
-        int number = scanner.nextInt();
         System.out.println("Enter your number");
+        int number = scanner.nextInt();
+
 
         if (number < 0) {
             System.out.println("Error ,the number is must be greater or equal to zero");
-            return;
+            return; ///Exits the program if input is invalid
         }
 
         for (int i = 0; i < number; i++) {
             System.out.println(text);
         }
         scanner.close();
-        }
     }
+}
 
 
